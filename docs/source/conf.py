58f87123fd2dfecf0ca22871c2424bae8deab2f9
Dave Whipp <david.whipp@helsinki.fi>
2023-05-22 21:56:11 +0200

    Update autodoc paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Path setup
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../../tc1d/'))
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../tc1d/'))
+#sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information
 
