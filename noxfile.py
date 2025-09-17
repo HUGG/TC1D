ccb235e21f9fea4d4779c05f1a8df3cec6a3b789
Dave Whipp <davewhipp@users.noreply.github.com>
2025-09-17 08:42:36 +0300

    Create noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+import nox
+
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+def test(session):
+
+    # install
+    session.install(".[tests]")
+
+    # Run tests
+    session.run("pytest")
