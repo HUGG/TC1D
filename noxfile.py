53c5c99f865dbbd52bd4086f6999a35105cbcc04
Dave Whipp <david.whipp@helsinki.fi>
2025-09-17 09:59:23 +0300

    Add debug output (another test)

--- noxfile.py
+++ noxfile.py
@@ -6,17 +6,6 @@ def tests(session):
     # install
     session.install(".[tests]")
 
-    # See what is installed
-    import importlib.metadata
-
-    distributions = importlib.metadata.distributions()
-    installed_packages = []
-    for dist in distributions:
-        args = (dist.metadata["Name"], dist.version)
-        installed_packages.append(args)
-    installed_packages.sort()  # Sort the packages by name
-    for package_name, version in installed_packages:
-        print(f"{package_name}=={version}")
-
     # Run tests
+    session.run("pip list")
     session.run("pytest")
