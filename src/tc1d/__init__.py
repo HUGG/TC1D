8ffb41d3a988c6668bc4e3ea0f5de2c11799b444
Dave Whipp <davewhipp@users.noreply.github.com>
2025-09-17 09:30:50 +0300

    Update __init__.py

--- src/tc1d/__init__.py
+++ src/tc1d/__init__.py
@@ -1,6 +1,12 @@
-from .tc1d import init_params, prep_model
+import importlib.metadata
+import sys
+
+if hasattr(sys, '_called_from_test'):
+    # Test run, do nothing
+    pass
+else:
+    # Import normally
+    from .tc1d import init_params, prep_model
 
 # Versioning
-import importlib.metadata
-
 __version__ = importlib.metadata.version("tc1d")
