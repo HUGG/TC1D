d0f85b0b4e04965c4f384aee93aa1596710b56f7
Dave Whipp <david.whipp@helsinki.fi>
2025-09-17 09:38:02 +0300

    Remove pytest hack

--- src/tc1d/__init__.py
+++ src/tc1d/__init__.py
@@ -1,12 +1,5 @@
 import importlib.metadata
-import sys
-
-if hasattr(sys, '_called_from_test'):
-    # Test run, do nothing
-    pass
-else:
-    # Import normally
-    from .tc1d import init_params, prep_model
+from .tc1d import init_params, prep_model
 
 # Versioning
 __version__ = importlib.metadata.version("tc1d")
